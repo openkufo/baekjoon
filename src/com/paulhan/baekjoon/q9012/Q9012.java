ee9bf7f23b12dbe0a667cb678be4271281e736f1
paul Han <openkufo@gmail.com>
2023-01-16 02:44:28 +0900

    add Q9012

---
+++ src/com/paulhan/baekjoon/q9012/Q9012.java
@@ -0,0 +1,48 @@
+package com.paulhan.baekjoon.q9012;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Stack;
+
+public class Q9012 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(bufferedReader.readLine());
+        
+        for(int i = 0; i < T; i++){
+            char[] str = bufferedReader.readLine().toCharArray();
+            
+            if(isVps(str)){
+                bufferedWriter.write("YES\n");
+            } else {
+                bufferedWriter.write("NO\n");
+            }
+
+            bufferedWriter.flush();
+        }
+        bufferedWriter.close();
+        bufferedReader.close();
+    }
+
+    // Valid Parenthesis String
+    private static boolean isVps(char[] str){
+        Stack<String> parenthesisStack = new Stack<String>();
+        for(char parenthesis : str){
+            if(parenthesisStack.size() == 0 && parenthesis == ')'){
+                return false;
+            }
+            if(parenthesis == '('){
+                parenthesisStack.push(Character.toString(parenthesis));
+            } else if(parenthesis == ')'){
+                parenthesisStack.pop();
+            }
+        }
+        
+        return parenthesisStack.isEmpty();
+    }
+}
