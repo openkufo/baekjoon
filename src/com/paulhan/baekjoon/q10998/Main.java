417aa675c949830545875bd884234cd35784afb0
paul Han <openkufo@gmail.com>
2023-01-18 15:26:20 +0900

    add Q10998

--- src/com/paulhan/baekjoon/q10998/Main.java
+++ src/com/paulhan/baekjoon/q10998/Main.java
@@ -1,5 +1,22 @@
 package com.paulhan.baekjoon.q10998;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
 public class Main {
-    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int result = Integer.parseInt(st.nextToken()) * Integer.parseInt(st.nextToken());
+        bw.write(result + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
