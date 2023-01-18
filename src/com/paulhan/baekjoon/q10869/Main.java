015dbd024c4d85b6002b9754e4fd8d6aaf8da00f
paul Han <openkufo@gmail.com>
2023-01-18 15:37:44 +0900

    add Q10869

---
+++ src/com/paulhan/baekjoon/q10869/Main.java
@@ -0,0 +1,30 @@
+package com.paulhan.baekjoon.q10869;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        
+        StringBuilder sb = new StringBuilder();
+        sb.append(a+b).append("\n");
+        sb.append(a-b).append("\n");
+        sb.append(a*b).append("\n");
+        sb.append(a/b).append("\n");
+        sb.append(a%b).append("\n");
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
