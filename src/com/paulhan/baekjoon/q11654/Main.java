683f8ffd4246fab009cc601eb5c6976cdb2ef43d
paul Han <openkufo@gmail.com>
2023-02-21 15:12:18 +0900

    add Q11654

---
+++ src/com/paulhan/baekjoon/q11654/Main.java
@@ -0,0 +1,13 @@
+package com.paulhan.baekjoon.q11654;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main  {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int ascii = br.readLine().charAt(0);
+        System.out.println(ascii);
+    }
+}
